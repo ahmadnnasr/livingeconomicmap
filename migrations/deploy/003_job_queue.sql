3c1de5b7d4c53f4313d207276c73315324cbcdc8
ahmadnnasr <nasrahmad0620@gmail.com>
2026-07-25 14:43:59 -0400

    Update 003_job_queue.sql

--- migrations/deploy/003_job_queue.sql
+++ migrations/deploy/003_job_queue.sql
@@ -3,7 +3,8 @@ CREATE TABLE IF NOT EXISTS jobs (
 
     job_type TEXT NOT NULL,
 
-    status TEXT NOT NULL DEFAULT 'QUEUED',
+status TEXT NOT NULL DEFAULT 'QUEUED'
+    CHECK (status IN ('QUEUED', 'RUNNING', 'COMPLETED', 'FAILED')),
 
     payload JSONB NOT NULL DEFAULT '{}'::jsonb,
 
